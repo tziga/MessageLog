5a1ce231e6c8763ad5fe9a2cb982127f1a7a9ad6
tziga <ctziga@mail.ru>
2020-10-18 23:29:34 +0300

    correct_table

--- OraclePLSQL/table/table_messagelog.sql
+++ OraclePLSQL/table/table_messagelog.sql
@@ -3,10 +3,9 @@ create table messagelog(id         number(38)     not null,
                         objname    varchar2(60)   default null,
                         insertdate date           default sysdate,
                         msgcode    varchar2(10)   default null,
-                        msgtext    varchar2(500)  default null,
-                        paramvalue varchar2(500)  default null,
-                        backtrace  varchar2(4000) default null,
-                        constraint messagelog_pk primary key (id))
+                        msgtext    varchar2(4000)  default null,
+                        paramvalue varchar2(4000)  default null,
+                        constraint pk_messagelog_id primary key (id))
 partition by range (insertdate)
-interval (numtoyminterval(3, 'MONTH'))
-( partition p1 values less than (to_date('01.01.2020', 'DD.MM.YYYY')));
+  interval (numtoyminterval(3, 'MONTH'))
+    (partition p1 values less than (to_date('01.01.2020', 'DD.MM.YYYY')));
